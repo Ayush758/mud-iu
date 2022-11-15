b7a526412d19ec2dc41b2c0f45f03590d90e6b94
Aayush Verma <94169959+Ayush758@users.noreply.github.com>
2022-11-15 22:45:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,15 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-     }
+      version = "~> 3.0"
+    }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region     = "us-east-1"
 }
-
 resource "aws_instance" "ec2demo" {
   ami           = "ami-08c40ec9ead489470" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
