8791121a7ea33279df6fd5642f0a45e4471234af
Aayush Verma <94169959+Ayush758@users.noreply.github.com>
2022-11-15 17:52:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,8 @@
-#resource "aws_instance" "ec2_example" {
-
-#    ami = "ami-08c40ec9ead489470"
-#    instance_type = "t2.micro"
-#    key_name= "key"
-#}
-
-
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      #version = "~> 3.21" # Optional but recommended in production
-    }
+     }
   }
 }
 
