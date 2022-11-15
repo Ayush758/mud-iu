812f1862dd5b7b261843a15cb1f67dbe07221cd1
Aayush Verma <94169959+Ayush758@users.noreply.github.com>
2022-11-15 22:54:37 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,8 +9,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  profile = "practice"
+  #profile = "practice"
   region     = "us-east-1"
+  access_key = "AKIAQICOLVC5JKTKBB7E"
+secret_key = "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"
+}
+  
 }
 resource "aws_instance" "ec2demo" {
   ami           = "ami-08c40ec9ead489470" # Amazon Linux in us-east-1, update as per your region
